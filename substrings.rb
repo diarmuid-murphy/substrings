b151b9525555ae70a60d0972f5341c02e1fd3a07
Diarmuid Murphy <diarmuid.com@gmail.com>
2015-04-22 00:36:26 -0500

    complete

--- substrings.rb
+++ substrings.rb
@@ -2,21 +2,11 @@ def substrings(words, dictionary)
 	words = words.downcase.gsub(/[^a-z0-9\s]/, '')
 	result = Hash.new(0)
 	
-	puts words.inspect
-	
-	# puts words.include? "how"
-	
 	dictionary.each do |word|
-		if words.include? word
-			# puts word
-			puts word + " yep"
-			# result[word] += 1
-		else
-			# puts "nope"
-		end
+		result[word] = words.scan(word).count
 	end
 	
-	# puts result
+	puts result
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
